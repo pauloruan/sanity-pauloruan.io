21ac1e98f9699404d920e1443084b1799c98e0f0
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-10 20:06:48 -0300

    refactor: Updated `post` document type schema, renamed fields and added descriptions.

--- schemas/post.ts
+++ schemas/post.ts
@@ -1,65 +1,80 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from "sanity"
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "post",
+  title: "Posts",
+  description: "Content for a blog post.",
+  type: "document",
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
+      description: "The title of the post."
     }),
     defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "subtitle",
+      title: "Subtitle",
+      type: "string",
+      description: "The subtitle of the post."
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      description: "The slug of the post.",
       options: {
-        source: 'title',
-        maxLength: 96,
-      },
+        source: "title",
+        maxLength: 96
+      }
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
+      name: "date",
+      title: "Date",
+      type: "datetime",
+      description: "The date of the post.",
       options: {
-        hotspot: true,
-      },
+        dateFormat: "YYYY-MM-DD"
+      }
     }),
     defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
+      name: "author",
+      title: "Author",
+      type: "reference",
+      description: "The author of the post.",
+      to: { type: "author" }
     }),
     defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      description: "The categories of the post.",
+      of: [{ type: "reference", to: { type: "category" } }]
     }),
     defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+      description: "The body of the post."
     }),
+    defineField({
+      name: "published",
+      title: "Published",
+      description: "Whether the post is published.",
+      initialValue: false,
+      type: "boolean"
+    })
   ],
 
   preview: {
     select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+      title: "title",
+      author: "author.name",
+      media: "author.image"
     },
     prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
-    },
-  },
+      const { author } = selection
+      return { ...selection, subtitle: author && `by ${author}` }
+    }
+  }
 })
