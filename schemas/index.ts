3377f0c68e418e958f59973e0caed6b9f90c1090
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-10 20:15:17 -0300

    refactor: Rendered schemas.

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,9 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import about from "./about"
+import author from "./author"
+import blockContent from "./blockContent"
+import category from "./category"
+import post from "./post"
+import project from "./project"
+import technology from "./technology"
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, technology, project, about]
