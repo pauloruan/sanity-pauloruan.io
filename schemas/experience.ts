8c9f81199390da637ade63444d10f1f0783689fc
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-10-14 23:00:27 -0300

    feat: Add Experience Schema

---
+++ schemas/experience.ts
@@ -0,0 +1,85 @@
+import { defineField, defineType } from "sanity"
+
+export default defineType({
+  name: "experience",
+  title: "Experiences",
+  description: "Work experiences.",
+  type: "document",
+  fields: [
+    defineField({
+      name: "company",
+      title: "Company",
+      type: "string",
+      description: "The name of the company."
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      description: "The slug of the experience.",
+      options: {
+        source: "company",
+        maxLength: 96
+      }
+    }),
+    defineField({
+      name: "position",
+      title: "Position",
+      type: "string",
+      description: "The position of the experience."
+    }),
+    defineField({
+      name: "linkedinImageURL",
+      title: "LinkedIn Image URL",
+      type: "url",
+      description: "The URL of LinkedIn company image."
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+      description: "The description of the experience."
+    }),
+    defineField({
+      name: "requiredSkills",
+      title: "Required Skills",
+      type: "array",
+      of: [{ type: "reference", to: { type: "technology" } }],
+      description: "The required skills for the experience."
+    }),
+    defineField({
+      name: "startDate",
+      title: "Start Date",
+      type: "date",
+      options: {
+        dateFormat: "YYYY-MM"
+      },
+      description: "The start date of the experience."
+    }),
+    defineField({
+      name: "endDate",
+      title: "End Date",
+      type: "date",
+      options: {
+        dateFormat: "YYYY-MM"
+      },
+      description: "The end date of the experience."
+    }),
+    defineField({
+      name: "current",
+      title: "Current",
+      type: "boolean",
+      description: "Is this experience current?",
+      initialValue: false,
+      options: {
+        layout: "switch"
+      }
+    })
+  ],
+  preview: {
+    select: {
+      title: "company",
+      subtitle: "position"
+    }
+  }
+})
--- schemas/index.ts
+++ schemas/index.ts
@@ -2,8 +2,18 @@ import about from "./about"
 import author from "./author"
 import blockContent from "./blockContent"
 import category from "./category"
+import experience from "./experience"
 import post from "./post"
 import project from "./project"
 import technology from "./technology"
 
-export const schemaTypes = [post, author, category, blockContent, technology, project, about]
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  technology,
+  project,
+  about,
+  experience
+]
